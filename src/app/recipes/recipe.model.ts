0821e15b5e17ea05a75a20fdf2241a7a1446226b
Radwan <radwan@bmbgroup.com>
2024-01-23 17:03:55 +0200

    Shopping Edit fix

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,16 +1,3 @@
-/*export class Recipe {
-  public name!: string;
-  public description!: string;
-  public imgPath!: string;
-
-  public constractor(public name: string, desc: string, imgPath: string) {
-    this.name = name;
-    this.description = desc;
-    this.imgPath = imgPath;
-  }
-}
-*/
-
 export class Recipe {
   constructor(public name: string, public description: string, public imagePath: string) {}
 }
