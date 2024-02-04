9da57bdd0b9c9a739bd8597e6424a4d4ecb1218a
Radwan <radwan@bmbgroup.com>
2024-02-04 16:30:44 +0200

    update header

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -11,15 +11,15 @@ export class RecipeItemComponent implements OnInit{
   @Input() recipe!:Recipe;
   maxDescriptionLength = 1;
   showFullDescription = false;
-  
+
   ngOnInit(): void {
-    console.log(this.recipe);
+   // console.log(this.recipe);
   }
-  
+
   toggleDescription(recipe:ElementRef) {
         this.showFullDescription = !this.showFullDescription;
         if(this.showFullDescription)
         recipe.nativeElement.scrollIntoView({ behavior: 'smooth', block: 'start' });
     }
-    
+
 }
